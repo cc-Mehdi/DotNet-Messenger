e72e9e9b2072db85c60fe979ee990079a265237a
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2024-09-09 19:17:39 +0330

    :sparkles: Messenger is ready now

--- src/SignalRChat/SignalRChat/obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
+++ src/SignalRChat/SignalRChat/obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f600d8bf6d6b330de76b589327d7c38ef4adb9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
